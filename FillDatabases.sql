a37f4d9e976c395efebf58c671fdd4b5da3bd0b7
Vladislav Zhogol <vzhogol176@gmail.com>
2023-06-12 12:02:39 +0300

    Add more values and wrap insertion into transaction

--- FillDatabases.sql
+++ FillDatabases.sql
@@ -1,5 +1,7 @@
 use Grocery;
 go
-insert Products values (0, 'apple'), (1, 'tomato'), (2, 'fish'), (3, 'chicken'), (4, 'potato'), (5, 'chair'), (6, 'toothbrush')
-insert Categories values (0, 'fruits'), (1, 'meat'), (2, 'vegetables')
-insert ProductCategories values (0, 0), (1, 0), (2, 1), (3, 1), (4, 2), (5, null), (6, null)
+begin transaction;
+	insert into Products (Id, Name) values (0, 'apple'), (1, 'tomato'), (2, 'fish'), (3, 'chicken'), (4, 'potato'), (5, 'chair'), (6, 'toothbrush'), (7, 'PC');
+	insert into Categories (Id, Name) values (0, 'fruits'), (1, 'meat'), (2, 'vegetables'), (3, 'for home'), (4, 'furniture');
+	insert into ProductCategories (ProductId, CategorieId) values (0, 0), (1, 0), (2, 1), (3, 1), (4, 2), (5, 3), (5, 4), (6, null), (7, 'dfsfgfs');
+commit transaction;
